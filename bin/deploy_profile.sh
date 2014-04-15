1e7bc35057ca482e8cd6a61cbf62aebd08962a4a
Andy Davis <andy.davis@koanhealth.com>
2014-04-15 15:04:29 -0500

    script tweak

--- bin/deploy_profile.sh
+++ bin/deploy_profile.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+MY_HOME="$HOME"
 DEPLOY_HOME="$HOME/deploy"
 FILES="$DEPLOY_HOME/.profile.d/*.sh"
 
+HOME="$DEPLOY_HOME"
 for f in $FILES
-  HOME=$DEPLOY_HOME f
+  source "$f"
 do
+HOME="$MY_HOME"
+
